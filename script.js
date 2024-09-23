3d8ccbf74e969e86578ec18051b4e5066faec402
mehedihasan <mehedihasan645356@gmail.com>
2024-09-23 12:53:52 +0600

    tab toggle style and blog page

--- script.js
+++ script.js
@@ -19,18 +19,46 @@ function setOutputValueById(id){
 
 }
 
-//function for css class add_remove in toggle tab
+//function for css class add in toggle tab
 function tabToggleToCssAdd(id){
     document.getElementById(id).classList.add('hidden');
 }
-
+//function for css class remove in toggle tab
 function tabToggleToCssRemove(id){
     document.getElementById(id).classList.remove('hidden');
 }
 
 
+//function for tab toggle style switching
+function tabToggleBtnStyleSwitch1(id){
+    document.getElementById(id).classList.add('bg-[#b4f461]');
+    document.getElementById(id).classList.remove('text-[#111111B3]','border' ,'border-[#F1F1F1]');
+}
 
+//function for tab toggle style switching
+function tabToggleBtnStyleSwitch2(id){
+    document.getElementById(id).classList.remove('bg-[#b4f461]');
+    document.getElementById(id).classList.add('text-[#111111B3]','border' ,'border-[#F1F1F1]');
+}
 
+//tab toggle btn for donation tab
+document.getElementById('donation').addEventListener('click', function(){
+    tabToggleToCssRemove('mainPage');
+    tabToggleToCssAdd('historysec');
+    tabToggleBtnStyleSwitch1('donation');
+    tabToggleBtnStyleSwitch2('history')
+ })
+ 
+//tab toggle btn for history tab
+ document.getElementById('history').addEventListener('click', function(){
+    tabToggleToCssRemove('historysec');
+    tabToggleToCssAdd('mainPage');
+    tabToggleBtnStyleSwitch1('history');
+    tabToggleBtnStyleSwitch2('donation')
+   
+ })
+
+ // input and  validation
 document.getElementById('donateBtn1').addEventListener('click', function(){
     const inputValue= getInputValueById('input1');
     const outPutValue = parseFloat(setOutputValueById('outPutValue').innerText = inputValue);
@@ -45,3 +73,8 @@ document.getElementById('donateBtn1').addEventListener('click', function(){
     // validationForInput('input1');
 
 })
+
+// window chinging function
+function windowChange(){
+    window.location.href = "./blog.html";
+}
